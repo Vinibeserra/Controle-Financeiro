38eb4c773176d3ac83ba5a20e6de2d437b4d0870
Vinibeserra <beserravinicius68@gmail.com>
2026-01-15 23:32:14 -0300

    feat: initial backend setup with auth, jwt and refresh token

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import authRoutes from './modules/auth/auth.routes';
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/auth', authRoutes);
+
+
+export { app };
